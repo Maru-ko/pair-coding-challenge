30828504d35360a671765f91a0a5e8d2fde1cce6
mugenmirai <@Tokoloshe6F669>
2021-06-08 15:36:05 -0400

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const { body, validationResult } = require("express-validator");
+
+const store = require("connect-loki");
+
+const app = express();
+const LokiStore = store(session);
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(morgan("common"));
